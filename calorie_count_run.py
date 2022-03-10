1e3ffd6b2cb315de23b33819ae39ae74f8360034
BhargaviKallam <bhargavireddy.kallam@gmail.com>
2022-03-10 12:44:19 -0500

    Update on website

--- calorie_count_run.py
+++ calorie_count_run.py
@@ -1,19 +1,41 @@
 import os
 import webbrowser
+from sys import platform
+from applescript import tell
 
-#extracts the path of the specific file(i.e here calorie_count_run.py file)
-path = os.path.dirname(os.path.abspath(__file__))
+if platform == "darwin" or platform == "linux":
 
-#opens the command promt and navigate to the path
-os.system("start cmd /c cd path" )
+    #os.system("""osascript -e 'tell application "Terminal" to do script "ls -l"'""")
 
-#instals the requirements 
-os.system("start cmd /c pip install -r requirements.txt" )
+    #set what command you want to run here
+    Command_1 = 'cd path'
+    tell.app( 'Terminal', 'do script "' + Command_1 + '"')
 
-#atomatically runs app python file
-os.system("start cmd /c python app.py" )
+    Command_2 = 'pip install -r requirements.txt'
+    tell.app( 'Terminal', 'do script "' + Command_2 + '"')
+
+    Command_3 = 'python app.py'
+    tell.app( 'Terminal', 'do script "' + Command_3 + '"')
+
+    
+
+else:
+    #extracts the path of the specific file(i.e here calorie_count_run.py file)
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    #opens the command promt and navigate to the path
+    os.system("start cmd /c cd path" )
+
+    #instals the requirements 
+    os.system("start cmd /c pip install -r requirements.txt" )
+
+    #atomatically runs app python file
+    os.system("start cmd /k python app.py" )
+
 
 #opens the website in the default browser
 webbrowser.open('http://127.0.0.1:5000/')
+
+
 
 
