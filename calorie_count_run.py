9d35b7343f8f6b9cc8d4313d7ab7ffb02ad68df3
Rajitha Muthukrishnan <rajitha.muthukrishnan@gmail.com>
2022-03-10 13:36:08 -0500

    Update script

--- calorie_count_run.py
+++ calorie_count_run.py
@@ -3,17 +3,9 @@ import webbrowser
 
 #extracts the path of the specific file(i.e here calorie_count_run.py file)
 path = os.path.dirname(os.path.abspath(__file__))
-
-#opens the command promt and navigate to the path
-os.system("start cmd /c cd path" )
-
-#instals the requirements 
-os.system("start cmd /c pip install -r requirements.txt" )
-
-#atomatically runs app python file
-os.system("start cmd /c python app.py" )
-
-#opens the website in the default browser
+# Install required packages
+os.system('cd ' + path)
+os.system('pip install -r requirements.txt')
+# Host and launch the application
+os.system('python app.py')
 webbrowser.open('http://127.0.0.1:5000/')
-
-
